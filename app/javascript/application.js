35e226f3f45d572a4248fd8e83a677a85504970f
bobbybgit <120116335+bobbybgit@users.noreply.github.com>
2023-02-07 13:28:37 +0000

    CSS edits to new scheme & JS for tab system

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,36 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
+document.addEventListener("turbo:load", function(){
+
+    const activeTabStorage = localStorage.getItem('currentTab')
+    let tabs = document.querySelectorAll(".tab");
+
+    if(activeTabStorage){
+        tabs[activeTabStorage].style.borderBottomColor = "var(--highlight)"
+        console.log(activeTabStorage)
+    }
+
+    console.log(tabs)
+
+    tabs.forEach((tab, pos, tabsPassed) => {
+        console.log(tabsPassed[pos]);
+        tab.addEventListener("click", function(){
+            tabs.forEach((tabStyle) => {
+                if (tabStyle === tab) {
+                    tabStyle.style.borderBottomColor = "var(--highlight)";
+                    localStorage.setItem('currentTab', pos)
+                } else {
+                    tabStyle.style.borderBottomColor = "var(--mainDark)";
+                }
+                console.log(`set ${tabStyle}`)
+            }) 
+        })
+    })
+    
+
+})
+
 
 
 
