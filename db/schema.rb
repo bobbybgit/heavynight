88b50c7f723ae43acb5637dc7bcee3011448380f
bobbybgit <120116335+bobbybgit@users.noreply.github.com>
2023-02-08 07:39:55 +0000

    Set up Devise and changed scoped views config

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_01_093329) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_08_073435) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "city"
@@ -22,6 +22,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_01_093329) do
     t.datetime "updated_at", null: false
     t.index ["event_setting_id"], name: "index_groups_on_event_setting_id"
     t.index ["location_id"], name: "index_groups_on_location_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "groups", "event_settings"
