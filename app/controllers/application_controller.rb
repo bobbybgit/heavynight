26d4ba94feac5b9f225211c437e3d7f8483aae94
bobbybgit <120116335+bobbybgit@users.noreply.github.com>
2023-02-20 08:26:55 +0000

    Made member list in groups centered

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
+  def generateCal
+    currentDate = Time.now 
+  end
+
   protected
   
   def configure_permitted_parameters
