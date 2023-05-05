ea08813732d5065688810826cc108c88e55624df
bobbybgit <120116335+bobbybgit@users.noreply.github.com>
2023-05-05 11:02:15 +0000

    started creating new cleaner layout

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -9,17 +9,24 @@ static targets = ['tab'];
 
   connect(){
     console.log("Tab Controller Connected");
-    this.tabTargets[0].style.borderBottomColor = "var(--highlight)"
+    this.tabTargets[0].style.backgroundColor = "var(--main)"
+    this.tabTargets[0].style.color = "var(--secondary)"
         
   }
 
   tabSelect(event){
+    let evTarg = event.target
+    while (!this.tabTargets.includes(evTarg)){
+      evTarg = evTarg.parentElement
+    }
     this.tabTargets.forEach((targ, i) => {
       console.log(targ)
-      if (targ != event.target){
-        targ.style.borderBottomColor = "var(--mainDark)"
+      if (targ != evTarg){
+        targ.style.backgroundColor = "var(--secondary)"
+        targ.style.color = "var(--main)"
       }else{
-        event.target.style.borderBottomColor = "var(--highlight)"
+        evTarg.style.backgroundColor = "var(--main)"
+        evTarg.style.color = "var(--secondary)"
       }
     });
     
